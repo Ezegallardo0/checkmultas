3d172aab9c686bd845d11aab6f30bb2ea23542ff
Ezegallardo0 <plumiferogaming@gmail.com>
2024-10-24 14:17:53 -0300

    "."

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import './App.css'
+import Navbar from './components/Navbar'
+import './styles/app.css'
 
 function App() {
 
   return (
     <>
-    
+      <Navbar />
+      <div className='homebg'>
+        <h2>Controlá tus multas, ahorrá plata</h2>
+        <p>Unite a miles de conductores que ahorran plata y evitan sorpresas.</p>
+        <div className='patente'>
+          <button className='Dni'>Ingresa tu Dni o Patente</button>
+          <button className='MA'>Consulta tus multas ahora</button>
+        </div>
+      </div>
     </>
   )
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import '../styles/nav.css'
+
+const Navbar = () => {
+    return (
+        <>
+            <div className='Nav'>
+                <h1>Check Multas</h1>
+                <div className='buton'>
+                    <button className='In'>Iniciar sesion</button>
+                    <button className='Cr'>Crear cuenta</button>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Navbar
