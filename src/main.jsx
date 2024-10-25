ee4183bb48fbb9cbdff13d1d47d11a1493d66988
Ezegallardo0 <plumiferogaming@gmail.com>
2024-10-25 15:29:12 -0300

    "."

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import 'bootswatch/dist/lux/bootstrap.min.css';
+import 'boxicons'
 import './index.css'
 import App from './App.jsx'
 
