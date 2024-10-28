7735ae826dbc29b12c71d13c94b555420681e836
Agustin Del Percio <114629649+N1t4z0@users.noreply.github.com>
2024-10-27 21:10:04 -0300

    login fix

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,12 +1,10 @@
-import React, { useState } from 'react'
-import '../styles/login.css'
-import Navbar from '../components/Navbar'
-import { Link } from 'react-router-dom'
-
+import React, { useState } from 'react';
+import '../styles/login.css';
+import Navbar from '../components/Navbar';
 
 const Login = () => {
-  const [email, setEmail] = useState ('')
-  const [contraseña, setContraseña ] = useState ('')
+  const [email, setEmail] = useState('');
+  const [contraseña, setContraseña] = useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -16,45 +14,46 @@ const Login = () => {
       contraseña
     });
   };
+
   return (
     <>
-    <Navbar />
-    <div className='container'>
-    <form className='form1' onSubmit={handleSubmit}>
-      <p className='title1'>Iniciar sesión</p>
-      <div>
-        <label>
-        <input
-          required
-          placeholder=""
-          type="email"
-          className="input1"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <span>Email</span>
-      </label>
+      <Navbar />
+      <div className='container'>
+        <form className='form' onSubmit={handleSubmit}>
+          <p className='title'>Iniciar sesión</p>
+          <div>
+            <label>
+              <input
+                required
+                placeholder=""
+                type="email"
+                className="input"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <span>Email</span>
+            </label>
 
-      <label>
-        <input
-          required
-          placeholder=""
-          type="password"
-          className="input2"
-          value={contraseña}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <span>Contraseña</span>
-      </label>
-        <button type='submit1' className='submit1'>Iniciar sesión</button>
-        <p className='signing1'>
-        ¿No tenes Cuenta todavia? <a className='reg' onClick={() => window.location.href = '/register'}>Crear Cuenta</a>
-        </p>
+            <label>
+              <input
+                required
+                placeholder=""
+                type="password"
+                className="input"
+                value={contraseña}
+                onChange={(e) => setContraseña(e.target.value)}
+              />
+              <span>Contraseña</span>
+            </label>
+            <button type='submit' className='submit'>Iniciar sesión</button>
+            <p className='signin'>
+              ¿No tenes Cuenta todavia? <a className='log' onClick={() => window.location.href = '/register'}>Crear Cuenta</a>
+            </p>
+          </div>
+        </form>
       </div>
-    </form>
-    </div>
     </>
-  )
+  );
 }
 
-export default Login
+export default Login;
