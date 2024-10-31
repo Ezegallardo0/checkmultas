4c9d2b07599d32c18febfbdfaba0af213e66f68a
Ezegallardo0 <plumiferogaming@gmail.com>
2024-10-31 13:01:53 -0300

    "added icons in the cards"

--- src/App.jsx
+++ src/App.jsx
@@ -84,13 +84,75 @@ function App() {
         </div>
         <button className="psr">+50,000 consultas realizadas este último mes</button>
       </section>
-
       <section className="Cards">
-        <Card title="Chequeá tu vehículo/moto" text="Ingresá tu patente o DNI para un escaneo gratis." className="cheq" />
-        <Card title="Revisá tu reporte" text="Fijate si tenés multas impagas y cuánto podés ahorrar." className="res" />
-        <Card title="Creá tu cuenta" text="Configurá el monitoreo gratis y accedé a descuentos exclusivos." className="crea" />
-      </section>
+        <div className="cheq">
+          <div className='Icon'>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="34"
+              height="34"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="lucide lucide-binoculars"
+            >
+              <path d="M10 10h4" />
+              <path d="M19 7V4a1 1 0 0 0-1-1h-2a1 1 0 0 0-1 1v3" />
+              <path d="M20 21a2 2 0 0 0 2-2v-3.851c0-1.39-2-2.962-2-4.829V8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v11a2 2 0 0 0 2 2z" />
+              <path d="M22 16 L2 16" />
+              <path d="M4 21a2 2 0 0 1-2-2v-3.851c0-1.39 2-2.962 2-4.829V8a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v11a2 2 0 0 1-2 2z" />
+              <path d="M9 7V4a1 1 0 0 0-1-1H6a1 1 0 0 0-1 1v3" />
+            </svg>
+          </div>
+          <Card className="card4" title="Chequeá tu vehículo/moto" text="Ingresá tu patente o DNI para un escaneo gratis." />
+        </div>
+        <div className="res">
+          <div className='Icon'>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="34"
+              height="34"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="lucide lucide-badge-dollar-sign"
+            >
+              <path d="M3.85 8.62a4 4 0 0 1 4.78-4.77 4 4 0 0 1 6.74 0 4 4 0 0 1 4.78 4.78 4 4 0 0 1 0 6.74 4 4 0 0 1-4.77 4.78 4 4 0 0 1-6.75 0 4 4 0 0 1-4.78-4.77 4 4 0 0 1 0-6.76Z" />
+              <path d="M16 8h-6a2 2 0 1 0 0 4h4a2 2 0 1 1 0 4H8" />
+              <path d="M12 18V6" />
+            </svg>
+          </div>
+          <Card className="card4" title="Revisá tu reporte" text="Fijate si tenés multas impagas y cuánto podés ahorrar." />
+        </div>
+        <div className="crea">
+          <div className='Icon'>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="34"
+              height="34"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="lucide lucide-user-plus"
+            >
+              <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
+              <circle cx="9" cy="7" r="4" />
+              <line x1="19" y1="8" x2="19" y2="14" />
+              <line x1="22" y1="11" x2="16" y2="11" />
+            </svg></div>
+          <Card className="card4" title="Creá tu cuenta" text="Configurá el monitoreo gratis y accedé a descuentos exclusivos." />
+        </div>
 
+      </section>
       <article className='card-container bg-container'>
         <section className="xq">
           <h1 className="pq">¿Por qué nos eligen?</h1>
@@ -98,13 +160,97 @@ function App() {
         </section>
         <section className="Cards2">
           <div className='div'>
-            <Card title="Monitoreo gratis 24/7" text="Mantenete al tanto de nuevas multas." className="card2" />
-            <Card title="Ahorrá hasta un 50%" text="Accedé a descuentos exclusivos con SinFotoMultas" className="card2"
-            />
+            <div className='card2'>
+              <div className='Icon1'>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="32"
+                  height="32"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="lucide lucide-radar"
+                >
+                  <path d="M19.07 4.93A10 10 0 0 0 6.99 3.34" />
+                  <path d="M4 6h.01" />
+                  <path d="M2.29 9.62A10 10 0 1 0 21.31 8.35" />
+                  <path d="M16.24 7.76A6 6 0 1 0 8.23 16.67" />
+                  <path d="M12 18h.01" />
+                  <path d="M17.99 11.66A6 6 0 0 1 15.77 16.67" />
+                  <circle cx="12" cy="12" r="2" />
+                  <path d="m13.41 10.59 5.66-5.66" />
+                </svg>
+              </div>
+              <Card className="XD" title="Monitoreo gratis 24/7" text="Mantenete al tanto de nuevas multas." />
+            </div>
+            <div className='card2'>
+              <div className="Icon1">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="32"
+                  height="32"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="lucide lucide-piggy-bank"
+                >
+                  <path d="M19 5c-1.5 0-2.8 1.4-3 2-3.5-1.5-11-.3-11 5 0 1.8 0 3 2 4.5V20h4v-2h3v2h4v-4c1-.5 1.7-1 2-2h2v-4h-2c0-1-.5-1.5-1-2V5z" />
+                  <path d="M2 9v1c0 1.1.9 2 2 2h1" />
+                  <path d="M16 11h.01" />
+                </svg>
+              </div>
+              <Card className="XD" title="Ahorrá hasta un 50%" text="Accedé a descuentos exclusivos con SinFotoMultas" /></div>
           </div>
           <div className='div'>
-            <Card title="Gestión familiar y de flotas" text="Monitoreá varios vehículos, DNI o motos en el mismo lugar." className="card2 card3" />
-            <Card title="Notificaciones al toque" text="Nunca más te pierdas una fecha de pago ni vencimiento." className="card2 card3" />
+            <div className='card2 card3'>
+              <div className="Icon1">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="32"
+                  height="32"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="lucide lucide-car"
+                >
+                  <path d="M19 17h2c.6 0 1-.4 1-1v-3c0-.9-.7-1.7-1.5-1.9C18.7 10.6 16 10 16 10s-1.3-1.4-2.2-2.3c-.5-.4-1.1-.7-1.8-.7H5c-.6 0-1.1.4-1.4.9l-1.4 2.9A3.7 3.7 0 0 0 2 12v4c0 .6.4 1 1 1h2" />
+                  <circle cx="7" cy="17" r="2" />
+                  <path d="M9 17h6" />
+                  <circle cx="17" cy="17" r="2" />
+                </svg>
+              </div>
+              <Card className="XD" title="Gestión familiar y de flotas" text="Monitoreá varios vehículos, DNI o motos en el mismo lugar." /></div>
+            <div className='card2 card3'>
+              <div className="Icon1">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="32"
+                  height="32"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="lucide lucide-bell-ring"
+                >
+                  <path d="M6 8a6 6 0 0 1 12 0c0 7 3 9 3 9H3s3-2 3-9" />
+                  <path d="M10.3 21a1.94 1.94 0 0 0 3.4 0" />
+                  <path d="M4 2C2.8 3.7 2 5.7 2 8" />
+                  <path d="M22 8c0-2.3-.8-4.3-2-6" />
+                </svg>
+              </div>
+              <Card className="XD" title="Notificaciones al toque" text="Nunca más te pierdas una fecha de pago ni vencimiento." />
+            </div>
           </div>
         </section>
       </article>
